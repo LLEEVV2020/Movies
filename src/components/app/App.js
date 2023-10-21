a6d4b561809f4ef9107338a770ebfbe057565085
lev <lleevv2020@gmail.com>
2023-10-21 14:57:33 +0400

    add

---
+++ src/components/app/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import './app.css'
+
+class App extends Component {
+  render() {
+    return <section className="test">test</section>
+  }
+}
+export default App
