70f5450e0124cb1e8e076fa68bb65e6199142113
lev <lleevv2020@gmail.com>
2023-10-25 20:14:13 +0400

    add

--- src/components/movies-list/movies-list.js
+++ src/components/movies-list/movies-list.js
@@ -1,26 +1,62 @@
+import { Component } from 'react'
 import { Space, Typography } from 'antd'
 
+import GeneralApiService from '../../services/api'
 import './movies-list.css'
 import Movie from '../movie'
 
 const { Text } = Typography
 
-function MoviesList() {
-  return (
-    <ul className="movies__list">
-      <Space direction="vertical" style={{ textAlign: 'center', width: '100%' }}>
-        <Text type="secondary">There are no films matching your request.</Text>
-        {Math.floor(Math.random() * 19)}
-      </Space>
+class MoviesList extends Component {
+  apiService = new GeneralApiService()
 
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-    </ul>
-  )
+  constructor() {
+    super()
+    this.updateFilms()
+  }
+
+  state = {
+    films: null,
+  }
+
+  updateFilms() {
+    this.apiService
+      .getFilms()
+      .then((films) => {
+        /*this.setState(({ films }) => {
+          return {
+            films: films,
+          }
+        })*/
+        console.log(films)
+        //console.log(this.state.films)
+      })
+      .catch((err) => {
+        console.error('Отсутствие фильмов', err)
+      })
+  }
+
+  render() {
+    return (
+      <ul className="movies__list">
+        <Space direction="vertical" style={{ textAlign: 'center', width: '100%' }}>
+          <Text type="secondary">There are no films matching your request.</Text>
+          {Math.floor(Math.random() * 19)}
+        </Space>
+        {
+          //this.state.films.map((film) => {
+          // return film
+          //})
+        }
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
+      </ul>
+    )
+  }
 }
 
 export default MoviesList
